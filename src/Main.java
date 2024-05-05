7349f0dc9a456984dd32d044eef49e5b4ab30730
Dar2090 <damh2090@gmail.com>
2024-05-05 20:56:11 -0300

    first-branch creation and package modelo creation w/Title, Serie and Movie class

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,40 @@
-import java.util.Scanner;
+import modelo.Movie;
+import modelo.Serie;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Hello world!");
-        // System.out.println("Película Matrix");
+        Principal principal = new Principal();
+        principal.muestraElMenu();
 
-        // Declaracion de variables:
-        // int, double, boolean son variables primitivas.
-        // String es una clase pero la usamos para guardar cadena de caracteres tambien, en comillas dobles.
-
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico = true;
-        String nombre = "Matrix";
-        String sinopsis = """
-                La mejor pelicula del fin del milenio,
-                la actuacion de Keanu es muy buena.
-                """;
-        double mediaEvaluacionesUsuario = 0;
-
-        System.out.println("Pelicula: " + nombre);
-        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Evaluacion: " + evaluacion);
-        System.out.println("Incluido en el plan basico: " + incluidoEnElPlanBasico);
-
-        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
-
-        System.out.println("Media de la evaluacion de " + nombre + ": " +  mediaEvaluacion);
-
-        if (fechaDeLanzamiento >= 2023){
-            System.out.println("Pelicula popular del momento");
-        }
-        else {
-            System.out.println("Pelicula Retro que vale la pena ver.");
-        }
-
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingresa la nota que le darias a Matrix: ");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionesUsuario = mediaEvaluacionesUsuario + notaMatrix;
-        }
-        System.out.println("La media de la pelicula " +
-                "Matrix calculada por el usuario es: " + (mediaEvaluacionesUsuario/3));
+//        Movie matrix = new Movie();
+//
+//        /*matrix.nombre = "Matrix";
+//        matrix.tiempoDeDuracionEnMinutos = 120;
+//        matrix.fechaDeLanzamiento = 1999;
+//        System.out.println(matrix.nombre);
+//        System.out.println(matrix.tiempoDeDuracionEnMinutos);
+//        System.out.println(matrix.fechaDeLanzamiento);
+//        matrix.muestraFichaTecnica();*/
+//
+//        matrix.setNombre("Matrix");
+//        matrix.setTiempoDeDuracionEnMinutos(120);
+//        matrix.setFechaDeLanzamiento(1999);
+//        matrix.muestraFichaTecnica();
+//
+//
+//        Movie encanto = new Movie();
+//        encanto.setNombre("Encanto");
+//        encanto.setTiempoDeDuracionEnMinutos(120);
+//        encanto.setFechaDeLanzamiento(2022);
+//        encanto.muestraFichaTecnica();
+//
+//        Serie friends = new Serie();
+//        friends.setNombre("Friends");
+//        friends.setFechaDeLanzamiento(1994);
+//        friends.setTemporadas(10);
+//        friends.setEpisodiosPorTemporada(22);
+//        friends.setDuracionPorEpisodio(30);
+//        friends.muestraFichaTecnica();
 
     }
 }
